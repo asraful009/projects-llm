bd4752cc71fb1debfba7ecd05eb1decaa76db24f
Asraful Islam <pavel@irinfosys.com>
2026-05-07 19:21:28 +0600

    file and chunk  save in db

--- rag/main.py
+++ rag/main.py
@@ -4,6 +4,8 @@ import os
 from transformers import AutoTokenizer
 import os
 from dotenv import load_dotenv
+import sqlite3
+from rab_db import RagDB
 
 load_dotenv()
 
@@ -20,18 +22,12 @@ def list_files_in_directory(directory):
     and f.endswith(".txt")
   ]
 
-def read_file(file_path):
+def read_file(directory, file_path):
   text = None
-  with open(file_path, "r", encoding="utf-8") as f:
+  full_path = os.path.join(directory, file_path)
+  with open(full_path, "r", encoding="utf-8") as f:
     text = f.read().strip()
   return text if text else None
-
-def read_all_files_in_directory(directory):
-  data = []
-  files = list_files_in_directory(directory)
-  for file in files:
-    data.append(read_file(os.path.join(directory, file)))
-  return data
 
 def split_into_chunks(text, max_tokens=300, overlap=50):
   tokens = tokenizer.encode(text, add_special_tokens=False)
@@ -65,19 +61,37 @@ def save_chunks_to_files(chunks, output_dir=".chunks"):
       f.write(chunk)
 
 def main():
-  print("Hello, RAG!")
-  data = read_all_files_in_directory(".data")
-  print(f"Read {len(data)} files.")
+  root_path = ".data"
+  if not os.path.exists(root_path):
+    print(f"Directory {root_path} does not exist. Please create it and add .txt files to process.")
+    return
+  files = list_files_in_directory(root_path)
+  print(f"Read {len(files)} files.")
   chunks_all = []
-  for i, text in enumerate(data):
-    if not text:
+  rag_db = RagDB()
+  rag_db.create_table()
+  for i, file in enumerate(files):
+    if rag_db.get_documents_by_file_name(file) is not None:
+      print(f"Document with file name {file} already exists in the database. Skipping.")
       continue
-    chunks = large_chunk_splitter(text)
+    meta = {"file_name": file, "data": read_file(root_path, file)}
+    if not meta["data"]:
+      continue
+    
+    chunks = large_chunk_splitter(meta["data"])
     print(f"File {i} split into {len(chunks)} chunks.")
     chunks_all.extend(chunks)
-
+    document_id = rag_db.insert_document(file_name=f"{meta['file_name']}", total_chunks=len(chunks_all))
+    for j, chunk in enumerate(chunks):
+      token_count = len(tokenizer.encode(chunk, add_special_tokens=False))
+      rag_db.insert_chunk(
+        document_id=document_id,
+        chunk_text=chunk,
+        chunk_index=j,
+        token_count=token_count
+      )
   print(f"Total chunks: {len(chunks_all)}")
-  save_chunks_to_files(chunks_all)
+  rag_db.close()
 
 if __name__ == "__main__":
   main()
