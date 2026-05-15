163cc89c7dcf020293a0ef05313823fd6b97a947
Asraful Islam <pavel@irinfosys.com>
2026-05-15 16:44:16 +0600

    chuck -> refine chunk : 2 times [ remove new line] chucks to ai for meta info

--- main.py
+++ main.py
@@ -9,34 +9,38 @@ def save_to_file(data, filename="chunks.json"):
 
 def main():
   rag = Rag_2()
-  chuck_for_token = []
-  if not os.path.isfile(".ai_data/chunks_pargraph.json"):
+  meta = {}
+  if not os.path.isfile(".ai_data/chuck_meta.json"):
+    chuck_for_token = []
+    if not os.path.isfile(".ai_data/chunks_pargraph.json"):
+      documents = rag.read_files()
+      print(len(documents))
+      paragraphs = rag.chunk_document(documents[0])
+      print(len(paragraphs))
+      paragraphs_chunks = rag.chunk_paragraphs(paragraphs)
+      print(len(paragraphs_chunks))
 
-    documents = rag.read_files()
-    print(len(documents))
+      save_to_file(paragraphs_chunks, ".ai_data/chunks_pargraph.json")
 
+      for paragraph in paragraphs_chunks:
+        chucks = rag.chunk_paragraph(paragraph)
+        chucks = rag.chunk_for_token(chucks)
+        chuck_for_token.extend(chucks)
 
-    paragraphs = rag.chunk_document(documents[0])
-    print(len(paragraphs))
-    paragraphs_chunks = rag.chunk_paragraphs(paragraphs)
-    print(len(paragraphs_chunks))
+      save_to_file(chuck_for_token, ".ai_data/chuck_for_token.json")
 
-    save_to_file(paragraphs_chunks, ".ai_data/chunks_pargraph.json")
+    else:
+      with open('.ai_data/chuck_for_token.json', 'r') as file:
+        chuck_for_token = json.load(file)
 
-    for paragraph in paragraphs_chunks:
-      chucks = rag.chunk_paragraph(paragraph)
-      chucks = rag.chunk_for_token(chucks)
-      chuck_for_token.extend(chucks)
-
-    save_to_file(chuck_for_token, ".ai_data/chuck_for_token.json")
-
+    print(len(chuck_for_token))
+    meta = rag.extract_meta_chunks(chuck_for_token)
+    save_to_file(meta, ".ai_data/chuck_meta.json")
   else:
-    with open('.ai_data/chuck_for_token.json', 'r') as file:
-      chuck_for_token = json.load(file)
+    with open('.ai_data/chuck_meta.json', 'r') as file:
+      meta = json.load(file)
 
-  print(len(chuck_for_token))
-  meta = rag.extract_meta_chunks(chuck_for_token)
-  save_to_file(meta, ".ai_data/chuck_meta.json")
+  print(meta)
 
 if __name__ == "__main__":
   main()
