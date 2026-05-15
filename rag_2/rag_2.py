32ef0d0b0cb825af7df63f946a07f0cceab7fc71
Asraful Islam <pavel@irinfosys.com>
2026-05-15 17:09:42 +0600

    embed prompt

--- rag_2/rag_2.py
+++ rag_2/rag_2.py
@@ -10,10 +10,10 @@ from chunking.token_chunker import TokenChunker
 
 class Rag_2:
   def __init__(self):
-    self.__recursiveChunking = RecursiveChunking()
-    self.__semanticRefinement = SemanticRefinement()
-    self.__tokenChunker = TokenChunker()
-    self.__localLLMChunking = LocalLLMChunking()
+    self.__recursiveChunking = None
+    self.__semanticRefinement = None
+    self.__tokenChunker = None
+    self.__localLLMChunking = None
 
   def read_files(self):
     reader = FileReader()
@@ -25,16 +25,24 @@ class Rag_2:
     return documents
 
   def chunk_document(self, document):
+    if not self.__recursiveChunking:
+      self.__recursiveChunking = RecursiveChunking()
     paragraphs =  self.__recursiveChunking.split_document(document)
     return paragraphs
 
   def chunk_paragraphs(self, paragraphs: list[str]) -> list[str]:
+    if not self.__semanticRefinement:
+      self.__semanticRefinement = SemanticRefinement()
     return self.__semanticRefinement.refine_chunks(paragraphs)
 
   def chunk_paragraph(self, paragraph: str) -> list[str]:
+    if not self.__recursiveChunking:
+      self.__recursiveChunking = RecursiveChunking()
     return self.__recursiveChunking.split_paragraph(paragraph)
 
   def chunk_for_token(self, chunks: list[str]) -> list[str]:
+    if not self.__tokenChunker:
+      self.__tokenChunker = TokenChunker()
     refine_chuck = []
     for chunk in chunks:
       c = self.__tokenChunker.chunk_text(chunk)
@@ -42,4 +50,6 @@ class Rag_2:
     return refine_chuck
 
   def extract_meta_chunks(self, chunks: list[str]):
+    if not self.__localLLMChunking:
+      self.__localLLMChunking = LocalLLMChunking()
     return self.__localLLMChunking.extract_add_more_info(chunks)
