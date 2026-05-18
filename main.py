cfaeef1e558b0b7e6d08c7806ad1b5082e52f63f
Asraful Islam <pavel@irinfosys.com>
2026-05-18 22:34:40 +0600

    faq

--- main.py
+++ main.py
@@ -10,40 +10,56 @@ def save_to_file(data, filename="chunks.json"):
 def main():
   rag = Rag_2()
   meta = {}
-  if not os.path.isfile(".ai_data/chuck_meta.json"):
-    chuck_for_token = []
-    if not os.path.isfile(".ai_data/chunks_pargraph.json"):
-      documents = rag.read_files()
-      print(len(documents))
-      paragraphs = rag.chunk_document(documents[0])
-      print(len(paragraphs))
-      paragraphs_chunks = rag.chunk_paragraphs(paragraphs)
-      print(len(paragraphs_chunks))
+  meta_faq = {}
+  if not os.path.isfile(".ai_data/chuck_for_faqs.json"):
+    if not os.path.isfile(".ai_data/chuck_meta.json"):
+      chuck_for_token = []
+      if not os.path.isfile(".ai_data/chunks_pargraph.json"):
+        documents = rag.read_files()
+        print(len(documents))
+        paragraphs = rag.chunk_document(documents[0])
+        print(len(paragraphs))
+        paragraphs_chunks = rag.chunk_paragraphs(paragraphs)
+        print(len(paragraphs_chunks))
 
-      save_to_file(paragraphs_chunks, ".ai_data/chunks_pargraph.json")
+        save_to_file(paragraphs_chunks, ".ai_data/chunks_pargraph.json")
 
-      for paragraph in paragraphs_chunks:
-        chucks = rag.chunk_paragraph(paragraph)
-        chucks = rag.chunk_for_token(chucks)
-        chuck_for_token.extend(chucks)
+        for paragraph in paragraphs_chunks:
+          chucks = rag.chunk_paragraph(paragraph)
+          chucks = rag.chunk_for_token(chucks)
+          chuck_for_token.extend(chucks)
 
-      save_to_file(chuck_for_token, ".ai_data/chuck_for_token.json")
+        save_to_file(chuck_for_token, ".ai_data/chuck_for_token.json")
 
+      else:
+        with open('.ai_data/chuck_for_token.json', 'r') as file:
+          chuck_for_token = json.load(file)
+
+      print(len(chuck_for_token))
+      meta = rag.extract_meta_chunks(chuck_for_token)
+      save_to_file(meta, ".ai_data/chuck_meta.json")
     else:
-      with open('.ai_data/chuck_for_token.json', 'r') as file:
-        chuck_for_token = json.load(file)
+      with open('.ai_data/chuck_meta.json', 'r') as file:
+        meta = json.load(file)
 
-    print(len(chuck_for_token))
-    meta = rag.extract_meta_chunks(chuck_for_token)
-    save_to_file(meta, ".ai_data/chuck_meta.json")
+    # print(meta)
+    meta_faq = rag.extract_faqs(meta)
+    print(meta_faq)
+    save_to_file(meta_faq, ".ai_data/chuck_for_faqs.json")
   else:
+    with open('.ai_data/chuck_for_faqs.json', 'r') as file:
+      meta_faq = json.load(file)
     with open('.ai_data/chuck_meta.json', 'r') as file:
       meta = json.load(file)
 
-  # print(meta)
-  meta_faq = rag.extract_faqs(meta)
-  print(meta_faq)
-  save_to_file(meta_faq, ".ai_data/chuck_for_faqs.json")
+  print(meta)
+  meta_embeddings = rag.generate_meta_for_embeddings(meta)
+  print(meta_embeddings)
+  save_to_file(meta_embeddings, ".ai_data/chuck_meta_embedding.json")
+  faq_embeddings = rag.generate_faq_for_embeddings(meta_faq)
+  # print(faq_embeddings)
+  save_to_file(faq_embeddings, ".ai_data/chuck_faq_embedding.json")
+
 
 if __name__ == "__main__":
   main()
--- rag_2/rag_2.py
+++ rag_2/rag_2.py
@@ -58,3 +58,51 @@ class Rag_2:
     if not self.__localLLMChunking:
       self.__localLLMChunking = LocalLLMChunking()
     return self.__localLLMChunking.extract_faqs_info(meta_info)
+
+  def generate_meta_for_embeddings(self, meta_info: dict):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    root_dir = os.path.dirname(base_dir)  # go up to projects-llm/
+    full_path = os.path.join(root_dir, "./prompt_templates/ai_prompt_template_for_embed.txt")
+    meta_info_template = ""
+    with open(full_path, "r", encoding="utf-8") as f:
+      meta_info_template = f.read()
+    meta_info_embedded_arr = []
+    if not meta_info["chunks"]:
+      return meta_info_embedded_arr
+    print(len(meta_info["chunks"]))
+    return []
+    for i, chunk in enumerate(meta_info["chunks"]):
+      embedding_text = meta_info_template.format(
+        chunk_type=chunk.get("chunk_type", ""),
+        title=chunk.get("title", ""),
+        summary=chunk.get("summary", ""),
+        chunk=chunk.get("chunk", ""),
+        keywords=", ".join(chunk.get("keywords", [])),
+        topics=", ".join(chunk.get("topics", [])),
+        people=", ".join(chunk.get("entities", {}).get("people", [])),
+        locations=", ".join(chunk.get("entities", {}).get("locations", [])),
+        orgs=", ".join(chunk.get("entities", {}).get("organizations", [])),
+        questions=", ".join(chunk.get("questions_raised", []))
+      )
+      meta_info_embedded_arr.append(embedding_text)
+    return meta_info_embedded_arr
+
+
+  def generate_faq_for_embeddings(self, faqs: dict):
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    root_dir = os.path.dirname(base_dir)  # go up to projects-llm/
+    full_path = os.path.join(root_dir, "./prompt_templates/ai_prompt_template_for_faq_embed.txt")
+    meta_info_template = ""
+    with open(full_path, "r", encoding="utf-8") as f:
+      meta_info_template = f.read()
+    meta_info_embedded_arr = []
+    if not faqs["FAQs"]:
+      return meta_info_embedded_arr
+    for i, chunk in enumerate(faqs["FAQs"]):
+      embedding_text = meta_info_template.format(
+        questions=chunk.get("faq", ""),
+        answer=chunk.get("ans", "")
+      )
+      meta_info_embedded_arr.append(embedding_text)
+    return meta_info_embedded_arr
+
